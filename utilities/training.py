592e9e9c37f4179b252f764b5397106cd4f20b72
danqiye1 <yedanqi@example.com>
2022-08-12 12:19:52 +0800

    WIP: train_epoch written but not tested

---
+++ utilities/training.py
@@ -0,0 +1,64 @@
+"""
+Utilities for training PyTorch models.
+
+@author: Ye Danqi
+"""
+import torch
+import copy
+from tqdm import tqdm
+
+def train_epoch(
+        model, dataloader,
+        immutable=True,
+        optimizer=None,
+        criterion=torch.nn.CrossEntropyLoss(),
+        device=torch.device("cpu"),
+    ):
+    """ Train one epoch of model.
+
+    Args:
+        model (torch.nn.Module): PyTorch model to be trained.
+        dataloader (torch.utils.data.DataLoader): PyTorch DataLoader of 
+            training data.
+        immutable (boolean): Flag for whether model is immutable. If True,
+            a new model will be instantiated with deepcopy.
+        optimizer (torch.optim.Optimizer): Optimizer for backpropagation.
+            Defaults to None.
+        criterion (torch.nn.Module): PyTorch loss function.
+        device (torch.device): The device for training.
+
+    Returns:
+        model (torch.nn.Module): A model that is trained. If immutable=False, this is
+            the reference to the original model.
+        loss (float): Average loss from this training epoch. 
+    """
+    if immutable:
+        model = copy.deepcopy(model)
+    model = model.to(device)
+
+    running_loss = 0.0
+    data_size = len(dataloader)
+
+    if not optimizer:
+        # Default optimizer if one is not provided
+        optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    
+    for i, data in tqdm(enumerate(dataloader), total=data_size):
+        imgs, labels = data
+        
+        optimizer.zero_grad()
+        
+        # Forward pass
+        outputs = model(imgs.to(device))
+        
+        # Compute loss and backpropagate error gradients
+        loss = criterion(outputs, labels.to(device))
+        loss.backward()
+        
+        # Gradient descent
+        optimizer.step()
+        
+        # Gather running loss
+        running_loss += loss.item()
+        
+    return (model, running_loss / data_size)
