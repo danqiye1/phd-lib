da6fbcba4532270406dec8c60900d98c015f2db3
danqiye1 <yedanqi@example.com>
2022-08-15 16:54:11 +0800

    Add validation function

--- utilities/eval.py
+++ utilities/eval.py
@@ -23,3 +23,35 @@ def calculate_error(logits, labels):
     predictions = torch.argmax(torch.softmax(logits, dim=1), dim=1)
     error_rate = (labels != predictions).sum() / len(labels)
     return error_rate.item()
+
+def validate(
+        model, dataloader, 
+        criterion=torch.nn.CrossEntropyLoss(),
+        device=torch.device("cpu")
+    ):
+    """Function for validating the model.
+
+    Args:
+        model (torch.nn.Module): PyTorch model to validate.
+        dataloader (torch.utils.data.DataLoader): DataLoader of validation set.
+        criterion (torch.nn.Module): Loss function for validation.
+        device (torch.device): CPU or GPU flag for training.
+
+    Returns:
+        avg_vloss (float): Average validation loss.
+        avg_verror (float): Average validation error.
+    """
+    running_vloss = 0.0
+    running_error = 0.0
+    for i, vdata in enumerate(dataloader):
+        v_inputs, v_labels = vdata
+        v_outputs = model(v_inputs.to(device))
+        error = calculate_error(v_outputs, v_labels.to(device))
+        running_error += error
+        v_loss = criterion(v_outputs, v_labels.to(device))
+        running_vloss += v_loss
+        
+    avg_vloss = running_vloss / (i + 1)
+    avg_verror = running_error / (i + 1)
+    
+    return (avg_vloss, avg_verror)
