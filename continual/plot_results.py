d5e73ea8f6167d866be098fb8bc5ee6166d80018
danqiye1 <yedanqi@example.com>
2022-09-22 10:29:33 +0800

    Plot all results in same task

--- continual/plot_results.py
+++ continual/plot_results.py
@@ -7,14 +7,12 @@ import argparse
 from matplotlib import pyplot as plt
 from .utils import plot_task_error
 
-from pdb import set_trace as bp
-
 parser = argparse.ArgumentParser()
 parser.add_argument('--results_dir', type=str, default='results')
 parser.add_argument('--epochs', type=str, default='5', choices=['3', '5'])
 parser.add_argument('--benchmark', type=str, default='splitmnist', choices=['splitmnist', 'permutedmnist'])
 parser.add_argument('--strategy', type=str, default='all', 
-                    choices=['all', 'ewc', 'pseudo', 'rehearsal', 'multihead'])
+                    choices=['all', 'ewc', 'pseudo', 'rehearsal', 'multihead_smooth'])
 args = parser.parse_args()
 
 # Preprocess path to results
@@ -26,25 +24,23 @@ with open(f'{results_dir}/boundaries.json', 'r') as fp:
 
 num_tasks = len(boundaries)
 
-if args.strategy == 'all':
-    for strategy in  ('ewc', 'pseudo', 'rehearsal', 'multihead'):
-        filepath = os.path.join(results_dir, f'{strategy}_error.json')
-
-        with open(filepath, 'r') as fp:
-            result = json.load(fp)
-
-        for task in result.keys():
-            plot_task_error(task, result, boundaries=boundaries, save=False, strategy=strategy)
-else:
-    filepath = os.path.join(results_dir, f'{args.strategy}_error.json')
+def plot_strategy(strategy, results_dir, boundaries):
+    filepath = os.path.join(results_dir, f'{strategy}_error.json')
 
     with open(filepath, 'r') as fp:
         result = json.load(fp)
 
     for task in result.keys():
-        plot_task_error(task, result, boundaries=boundaries, save=False, strategy=args.strategy)
+        plot_task_error(task, result, boundaries=boundaries, save=False, strategy=strategy)
+
+if args.strategy == 'all':
+    for strategy in  ('ewc', 'pseudo', 'rehearsal', 'multihead'):
+       plot_strategy(strategy, results_dir, boundaries)
+else:
+    plot_strategy(args.strategy, results_dir, boundaries)
 
 
 for task in range(num_tasks):
     plt.figure(task + 1)
-    plt.savefig("")
+    plt.legend()
+    plt.savefig(f"{results_dir}/task{task}-{args.epochs}epochs-{args.benchmark}.jpg")
--- continual/utils/plotting.py
+++ continual/utils/plotting.py
@@ -1,6 +1,7 @@
 """
 Plotting utilities
 """
+import numpy as np
 from matplotlib import pyplot as plt
 
 def plot_task_error(
@@ -11,11 +12,18 @@ def plot_task_error(
     ):
     plt.figure(task_num)
     plt.title(f'Error for Task {task_num}')
-    plt.plot(val_error[task_num], label=strategy)
     plt.xlabel("iterations")
     plt.ylabel("error")
     for x in boundaries:
         plt.vlines(x, ymin=0, ymax=1, color='r', linestyles="dashed")
+    
+    # Create the x-values from xstart to xend
+    # xend is always the total length of the first task
+    # xstart is whatever boundary of the previous task
+    xstart = (boundaries[int(task_num) - 1] if int(task_num) else 0)
+    xend = len(val_error['0'])
+    xaxis = np.arange(xstart, xend)
+    plt.plot(xaxis, val_error[task_num], label=strategy)
     
     if save:
         plt.savefig(f'{strategy}.jpg')
